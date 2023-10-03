4761e5097e7440080c483e763d07b9879c340761
Peter Østergaard <peter@engelund.eu>
2023-10-03 11:53:35 +0200

    Backend listens on 0.0.0.0:5000

--- FIDS.Backend/Program.cs
+++ FIDS.Backend/Program.cs
@@ -1,16 +1,13 @@
-
 using FIDS.Backend.Hubs;
 using FIDS.Backend.Services;
 using FIDS.Backend.Workers;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSignalR();
-builder.Services.AddTransient<IBoardingService, BoardingService>();
-builder.Services.AddTransient<IBaggageService, BaggageService>();
-builder.Services.AddTransient<IFlightService, FlightService>();
+builder.Services.AddSingleton<IBoardingService, BoardingService>();
+builder.Services.AddSingleton<IBaggageService, BaggageService>();
+builder.Services.AddSingleton<IFlightService, FlightService>();
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -32,4 +29,4 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.Run();
+app.Run("http://0.0.0.0:5000");
