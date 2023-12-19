a2ce5cb527150812967f7893936020745588b828
Elvirgls <elvir.fejzic@gls-denmark.com>
2023-12-19 20:27:56 +0100

    arrival client changed

---
+++ FIDS.ArrivalClient.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace FIDS.ArrivalClient.Tests;
+using FluentAssertions;
+using NSubstitute;
+public class UnitTest1
+{
+    [Fact]
+    public void TestArrivalsController_GetAllArrivals()
+    {
+        // Arrange
+        var flyTest = new FlyTest();
+
+        // Act
+        var result = flyTest.ValidateFliteId(1);
+
+        // Assert
+        result.Should().Be(1);
+    }
+}
+public  class FlyTest
+{
+    public  int ValidateFliteId(int id)
+    {
+
+        return 2;
+    }
+}
